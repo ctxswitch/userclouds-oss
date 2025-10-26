6feb5c9bd5aaa11c05c95f3c0ac6b35bfe13da0d
Rob Lyon <rob@ctxswitch.com>
2025-10-26 12:47:33 -0700

    udpate

--- infra/secret/cache.go
+++ infra/secret/cache.go
@@ -14,14 +14,12 @@ type cacheObject struct {
 	Expires time.Time
 }
 
-// cache is an in-memory cache of secrets.
+// cache is an in-memory cache of secrets
 type cache struct {
 	secrets      map[string]cacheObject
 	secretsMutex sync.RWMutex
 }
 
-// Get returns a secret and a boolean value if it exists and has not reached its
-// expiration, otherwise it returns an empty string and a falsy "found" value.
 func (c *cache) Get(loc string) (string, bool) {
 	c.secretsMutex.RLock()
 	defer c.secretsMutex.RUnlock()
@@ -35,7 +33,6 @@ func (c *cache) Get(loc string) (string, bool) {
 	return "", false
 }
 
-// Store creates a secret in the cache and set an expiration on it.
 func (c *cache) Store(loc string, secret string) {
 	c.secretsMutex.Lock()
 	defer c.secretsMutex.Unlock()
@@ -46,7 +43,6 @@ func (c *cache) Store(loc string, secret string) {
 	}
 }
 
-// Reset resets the cache state to empty.
 func (c *cache) Reset() {
 	c.secrets = map[string]cacheObject{}
 }
